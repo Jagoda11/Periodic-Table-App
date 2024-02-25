eae51613398643188ad43b273b1a50f724db341b
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-02-25 12:19:22 +0100

    Update Jest and TypeScript configurations

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^src/(.*)$': '<rootDir>/src/$1',
@@ -8,9 +9,10 @@ module.exports = {
     '^components/(.*)$': '<rootDir>/src/components/$1'
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  moduleDirectories: ['node_modules', 'src'],
   transform: {
-    '^.+\\.ts$': 'ts-jest', // Transforms TypeScript files using ts-jest
-    '.*\\.(vue)$': '@vue/vue3-jest', // Transforms Vue files using @vue/vue3-jest
+    '^.+\\.ts$': ['ts-jest', { diagnostics: false, isolatedModules: true }],
+    '.*\\.(vue)$': '@vue/vue3-jest',
     '^.+\\js$': 'babel-jest'
   },
   testPathIgnorePatterns: ['/node_modules/', '/playwright-tests/', '/cypress-tests/', '/dist/']
