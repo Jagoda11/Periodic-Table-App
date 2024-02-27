3dcd31883944c58505c79422231d4aa5cf7b7c60
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-02-27 22:19:19 +0100

    Update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 //import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [vue(), vueJsx()],
+  plugins: [vue({}), vueJsx()],
   resolve: {
     alias: {
       '@': 'src'
