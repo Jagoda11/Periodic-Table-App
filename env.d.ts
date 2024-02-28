54d2e7cfa66fddac4b8b34dba4f1d3497d95bc9a
Jagoda11 <jagoda.cubrilo@yahoo.com>
2024-02-28 20:33:39 +0100

    Add Babel configuration and environment type definitions

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
+/// <reference types="vite-plugin-pages/client" />
+
+interface ImportMetaEnv {
+  // Define the types for your environment variables, e.g.,
+  readonly VITE_APP_TITLE: string
+  readonly VITE_API_BASE_URL: string
+
+  // If you have boolean or number environment variables, you might define them like so:
+  readonly VITE_FEATURE_FLAG_ENABLED: boolean
+  readonly VITE_API_TIMEOUT_MS: number
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- jest.setup.ts
+++ jest.setup.ts
@@ -2,15 +2,26 @@
 import { createApp } from 'vue'
 import { config } from '@vue/test-utils'
 import { createPinia } from 'pinia'
-import type { Router } from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
 
 // Setup Pinia
 config.global.plugins.push(createPinia())
 
-// Setup Vue Router if your components depend on routing
-const router: Router = createRouter({
-  history: createWebHistory(),
-  routes: [] // Define your routes here if necessary
-})
-config.global.plugins.push(router)
+// Mocking Vue Router to avoid test errors
+jest.mock('vue-router', () => ({
+  createRouter: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn()
+    // Add other router methods as needed for your tests
+  })),
+  createWebHistory: jest.fn(),
+  // If component uses useRoute or useRouter, mock them here
+  useRouter: () => ({
+    push: jest.fn()
+    // add other methods as necessary
+  }),
+  useRoute: () => ({
+    params: {},
+    query: {}
+    // add other route properties as necessary
+  })
+}))
