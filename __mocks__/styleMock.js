91c16a930fe0caa062bb9c01b6c9c7ea7b5a635c
Jagoda11 <Jagoda11@users.noreply.github.com>
2024-06-22 18:15:20 +0000

    deploy: a3e97b1bfab3f8b2704494bc7ff160aa1c45a835

--- __mocks__/styleMock.js
+++
@@ -1,3 +0,0 @@
-/* eslint-env node */
-/* eslint-disable no-unused-vars */
-module.exports = {}
--- src/types/counter.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-
-export interface CounterStore {
-  count: Ref<number>
-  doubleCount: ComputedRef<number>
-  increment: () => void
-}
