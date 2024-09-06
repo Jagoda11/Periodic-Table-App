f920d77e40d115f5092978baab14a1fba20062b6
Jagoda11 <Jagoda11@users.noreply.github.com>
2024-09-06 20:21:59 +0000

    deploy: 80859921b2df187f8f887b930bfcd4ca93cca3d7

---
+++ assets/AboutView-CWvonZ3c.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as t,a}from"./index-C-Tk5WjO.js";const n={},c={class:"about"};function r(_,e){return s(),t("div",c,e[0]||(e[0]=[a("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",r]]);export{l as default};
